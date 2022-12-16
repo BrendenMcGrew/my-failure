e675dd574fdb33a63d316649c4e9a646e932eb35
BrendenMcGrew <brendenmcgrew658@gmail.com>
2022-12-16 08:34:37 -0600

    Added table component

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,8 +1,10 @@
 import Banner from "../../components/dashboard components/perosnal data banner";
+import Table from "../../components/dashboard components/personal files table"
 export default function Dashboard() {
     return(
         <>
             <Banner/>
+            <Table/>
         </>
     )
 }
