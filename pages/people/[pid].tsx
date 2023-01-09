1b7866383d00a017ffc4c58b5621c1ec372f9622
BrendenMcGrew <brendenmcgrew658@gmail.com>
2023-01-09 09:37:23 -0600

    Fixed it

--- pages/people/[pid].tsx
+++ pages/people/[pid].tsx
@@ -3,11 +3,24 @@ import PersonalFilesTable from "../../components/dashboard components/personal f
 import Banner from "../../components/dashboard components/perosnal data banner";
 // @ts-ignore
 import SwaggerClient from 'swagger-client';
+import {useEffect} from "react";
 
 export default function Post () {
     const router = useRouter()
     const { pid } = router.query
-    const personInfo = getPerson(pid);
+    let personInfo;
+
+    useEffect(() => {
+        new SwaggerClient({
+            url: 'https://dirt.af.mil/api.json',
+            authorizations: { tokenAuthn:
+                    window.sessionStorage.getItem('token')
+            }}).then((client:any) => client.apis.default.getSubInfo({pid}))
+            .then((Response: any) => {
+                // eslint-disable-next-line react-hooks/exhaustive-deps
+                personInfo = Response;
+            })
+    }, []);
 
     return (
         <>
@@ -15,15 +28,4 @@ export default function Post () {
             <PersonalFilesTable data={{}}/>
         </>
     )
-}
-
-function getPerson(personID:any): any {
-    new SwaggerClient({
-        url: 'https://dirt.af.mil/api.json',
-        authorizations: { tokenAuthn:
-                window.sessionStorage.getItem('token')
-        }}).then((client:any) => client.apis.default.getSubInfo({personID}))
-        .then((Response: any) => {
-            console.log()
-        })
 }
