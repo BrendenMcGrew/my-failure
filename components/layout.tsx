d2d893921f889892231b9d69c3e4b2a4caea330f
BrendenMcGrew <brendenmcgrew658@gmail.com>
2023-01-09 10:24:51 -0600

    Did it

--- components/layout.tsx
+++ components/layout.tsx
@@ -9,7 +9,7 @@ export default function Layout({ children }: any) {
     }, []);
     return (
         <>
-            {isLoggedIn && <Navbar/>}
+            {isLoggedIn && (<Navbar/>)}
             <main>{children}</main>
         </>
     )
